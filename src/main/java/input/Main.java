383c3e09d190fe123196ec3e86d85280a522c051
DESKTOP-N1D5NOF\User <kimdaesoop@gmail.com>
2018-12-31 19:52:33 +0900

    별찍기 3, 문제를 제대로 읽지 않아 많이 틀렸습니다.

--- src/main/java/input/Main.java
+++ src/main/java/input/Main.java
@@ -6,12 +6,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
-        for (int i = 1; i <= a; i++) {
-            if (i != 1) {
+        for (int i = a; i >= 1; i--) {
+            if (i != a) {
                 System.out.println();
-            }
-            for (int j = a - i; j >= 1; j--) {
-                System.out.print(" ");
             }
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
