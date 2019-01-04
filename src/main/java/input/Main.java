51a14b782d84cc256d390548cf1bedb576862953
DESKTOP-N1D5NOF\User <kimdaesoop@gmail.com>
2019-01-04 21:58:55 +0900

    백준 알고리즘 시험성적

--- src/main/java/input/Main.java
+++ src/main/java/input/Main.java
@@ -5,11 +5,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int A = scanner.nextInt();
-        if (A <= 100000) {
-            for (int i = A; i >= 1; i--) {
-                System.out.println(i);
-            }
+        int a = scanner.nextInt();
+        if (a <= 100 && a >= 90) {
+            System.out.println("A");
+        } else if (a <= 89 && a >= 80) {
+            System.out.println("B");
+        } else if (a <= 79 && a >= 70) {
+            System.out.println("C");
+        } else if (a <= 69 && a >= 60) {
+            System.out.println("D");
+        } else {
+            System.out.println("F");
         }
     }
 }
