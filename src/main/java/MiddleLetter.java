2afc0cfbffded8d7ce1461be9cc9253e382c3805
daesoop <kimdaesoop@hotmail.com>
2019-03-11 20:26:42 +0900

    프로그래머스 가운데 글자 가져오기 알고리즘/ 다른 사람의 풀이를 보니 substring을 사용해서 문자 가르기로 갖고 오는 방법이 있었다. 다음에 기회가 있을 때, substring을 사용해 보도록 해야겠다.

---
+++ src/main/java/MiddleLetter.java
@@ -0,0 +1,20 @@
+public class MiddleLetter {
+    public static void main(String[] args) {
+        String s = "qwer";
+        Solution8 solution = new Solution8();
+        System.out.println(solution.solution(s));
+    }
+}
+class Solution8 {
+    public String solution(String s) {
+        String answer = "";
+        String[] str = s.split("");
+        if (str.length % 2 == 0) {
+            answer += str[str.length / 2 - 1];
+            answer += str[str.length / 2 ];
+        } else if (str.length % 2 != 0) {
+            answer = str[(int) (str.length / 2 + 0.5)];
+        }
+        return answer;
+    }
+}
