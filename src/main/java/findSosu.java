185a497db21bb3aa157e3d8b526f70c2c356d6a9
daesoop <kimdaesoop@hotmail.com>
2019-03-14 18:47:11 +0900

    프로그래머스 소수찾기

---
+++ src/main/java/findSosu.java
@@ -0,0 +1,25 @@
+public class findSosu {
+    public static void main(String[] args) {
+        int i = 5;
+        Solution9 solution = new Solution9();
+        System.out.println(solution.solution(i));
+    }
+}
+class Solution9 {
+    public int solution(int num) {
+        int result = 0;
+        for (int i = 2; i <= num; i++) {
+            boolean right = true;
+            for (int j = 2; j * j <= i; j++) {
+                if (i % j == 0) {
+                    right = false;
+                    break;
+                }
+            }
+            if(right) {
+                result++;
+            }
+        }
+        return result;
+    }
+}
