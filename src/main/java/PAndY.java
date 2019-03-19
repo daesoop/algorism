95db6baf205d17d900c88c130908cf2f567a5a4e
daesoop <kimdaesoop@hotmail.com>
2019-03-19 23:46:30 +0900

    프로그래머스 문자열 내 p와 y의 개수/ 간단한 문제 였지만 생각이 깊었던 문제였다. 왠지 다시 공부해야할 거 같은...

---
+++ src/main/java/PAndY.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class PAndY {
+    public static void main(String[] args) {
+        String str = "pPoooy";
+        Solution10 solution = new Solution10();
+        System.out.println(solution.solution(str));
+    }
+}
+class Solution10 {
+    public boolean solution(String str) {
+        boolean answer = true;
+        int pCount = 0;
+        int yCount = 0;
+        String s = str.toLowerCase();
+        String[] strArr = s.split("");
+        for (int i = 0; i < s.length(); i++) {
+           if (strArr[i].equals("p")) {
+               pCount++;
+           }
+           if (strArr[i].equals("y")) {
+               yCount++;
+           }
+        }
+        answer = pCount == yCount;
+        return answer;
+    }
+}
