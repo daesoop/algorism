46cad9729ed39cda7242da9338de6bf235226501
daesoop <kimdaesoop@hotmail.com>
2019-04-26 18:03:50 +0900

    프로그래머스 완주하지 못한 선수 HashMap을 이용하여 성공

--- src/main/java/programers/level1/marathon.java
+++ src/main/java/programers/level1/marathon.java
@@ -1,6 +1,8 @@
 package programers.level1;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class marathon {
     public static void main(String[] args) {
@@ -13,15 +15,23 @@ public class marathon {
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        Arrays.sort(participant);
-        Arrays.sort(completion);
-        int i;
-        for (i = 0; i < completion.length; i++) {
-
-            if (!participant[i].equals(completion[i])) {
-                return participant[i];
+        Map<String, Integer> map = new HashMap<>();
+        String answer = "";
+        for (int i = 0; i < participant.length; i++) {
+            if (!map.containsKey(participant[i])) {
+                map.put(participant[i], 1);
+            } else if (map.containsKey(participant[i])) {
+                map.put(participant[i], map.get(participant[i]) + 1);
             }
         }
-        return participant[i];
+        for (int i = 0; i < completion.length; i++) {
+            map.put(completion[i] ,map.get(completion[i]) - 1);
+        }
+        for (String s : map.keySet()) {
+            if (map.get(s) == 1) {
+                answer = s;
+            }
+        }
+        return answer;
     }
 }
