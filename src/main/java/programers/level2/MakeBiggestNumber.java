3f82212fcacc3fa8e2b3d8ca3d7ff9cbed9b9dec
daesoop <kimdaesoop@hotmail.com>
2019-05-05 22:27:52 +0900

    프로그래머스 큰 수 만들기/ 굉장히 고민을 많이 했었다. 배열 안에 숫자들을 전부 다 집어 넣은 다음 정렬로 가장 큰 숫자를 계산하니 시간초과가 나서 고민하던 찰나 0번째 인덱스 부터 큰 수들만 뽑은다음 숫자들만 넣어주는 방법을 알게 되어 만들어 보았다.

--- src/main/java/programers/level2/MakeBiggestNumber.java
+++ src/main/java/programers/level2/MakeBiggestNumber.java
@@ -2,19 +2,31 @@ package programers.level2;
 
 public class MakeBiggestNumber {
     public static void main(String[] args) {
-        String number = "1924";
+        String number = "4177252841";
         Solution12 solution = new Solution12();
-        System.out.println(solution.solution(number, 2));
+        System.out.println(solution.solution(number, 4));
     }
 }
-class Solution12{
-    public String solution(String number, int k) {
-        String answer = "";
-        int length = number.length() - k;
-        String[] strArr = number.split("");
-        for (int i = 0; i < number.length(); i++) {
 
+class Solution12 {
+    public String solution(String number, int k) {
+        int idx = 0;
+        char max;
+        StringBuilder answer = new StringBuilder();
+
+        if (number.charAt(0) == '0') return "0";
+        for (int i = 0; i < number.length() - k; i++) {
+            max = '0';
+            System.out.println("i : " + i);
+            for (int j = idx; j <= k + i; j++) {
+                if (max < number.charAt(j)) {
+                    max = number.charAt(j);
+                    System.out.println("max : " + max);
+                    idx = j + 1;
+                }
+            }
+            answer.append(max);
         }
-        return answer;
+        return answer.toString();
     }
 }
