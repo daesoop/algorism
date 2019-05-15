5f698c6ce5da46dcde95be28a2af77eedebff16d
daesoop <kimdaesoop@hotmail.com>
2019-05-15 17:44:00 +0900

    Leet Code problem 14. Longest Common Prefix/ Was not easy to solve this problem. i got hint from one of user from Leet Code. Might be this is the best way solution. but I should find another way by myself

--- src/main/java/leetCode/AddTwoNumbers.java
+++ src/main/java/leetCode/AddTwoNumbers.java
@@ -1,13 +1,24 @@
 package leetCode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AddTwoNumbers {
     public static void main(String[] args) {
         Solution solution = new Solution();
-
-        solution.solution();
+        ListNode listNode1 = new ListNode(234);
+        ListNode listNode2 = new ListNode(564);
+        solution.solution(listNode1, listNode2);
     }
 }
 class Solution{
-    public void solution() {
+    public void solution(ListNode listNode1, ListNode listNode2) {
+
     }
 }
+
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+ }
---
+++ src/main/java/leetCode/LongestCommonPrefix.java
@@ -0,0 +1,25 @@
+package leetCode;
+
+public class LongestCommonPrefix {
+    public static void main(String[] args) {
+        Solution4 solution = new Solution4();
+        String[] strings = {"dog","racecar","car"};
+        System.out.println(solution.solution(strings));
+    }
+}
+
+class Solution4{
+    public String solution(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        for (int i = 0; i < strs[0].length(); i++) {
+            for (int j = 0; j < strs.length; j++) {
+                if (i >= strs[j].length() || strs[0].charAt(i) != strs[j].charAt(i)) {
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+        return strs[0];
+    }
+}
