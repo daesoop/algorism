ba3d043f54fdf648fd992021b214a938a30ec529
daesoop <kimdaesoop@hotmail.com>
2019-05-18 22:02:03 +0900

    Leet Code Problem 12 Integer to Roman/ I solved the problem Roman to Integer 2days ago. i thought solution was similar with Integer to Roman, but was not. i solved this with 1 int array and another string array put them numbers and strings. use foreach and while till smaller then int arrays. and it is working now. was hard to solve this because i trying use map. but was much easier use arrays.

---
+++ src/main/java/leetCode/IntegerToRoman.java
@@ -0,0 +1,28 @@
+package leetCode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class IntegerToRoman {
+    public static void main(String[] args) {
+        Solution7 solution = new Solution7();
+        System.out.println(solution.intToRoman(49));
+    }
+}
+
+class Solution7{
+    public String intToRoman(int num) {
+        int[] values = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String[] strs = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        StringBuilder answer = new StringBuilder();
+
+        for (int i = 0; i < values.length; i++) {
+            while (num >= values[i]) {
+                num -= values[i];
+                answer.append(strs[i]);
+            }
+        }
+
+        return answer.toString();
+    }
+}
--- src/main/java/leetCode/ReverseInteger.java
+++ src/main/java/leetCode/ReverseInteger.java
@@ -3,7 +3,7 @@ package leetCode;
 public class ReverseInteger {
     public static void main(String[] args) {
         Solution6 solution = new Solution6();
-        int num = 52341;
+        int num = -52341;
         System.out.println(solution.solution(num));
     }
 }
