d1344bbee927021260b218e460ef01df0012b394
soop <kimdaesoop@hotmail.com>
2020-02-20 18:08:26 +0900

    add two numbers without listNode

--- src/main/java/leetCode/AddTwoNumbers.java
+++ src/main/java/leetCode/AddTwoNumbers.java
@@ -1,19 +1,47 @@
 package leetCode;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class AddTwoNumbers {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        ListNode listNode1 = new ListNode(234);
-        ListNode listNode2 = new ListNode(564);
+        List<Integer> listNode1 = new LinkedList<>();
+        List<Integer> listNode2 = new LinkedList<>();
+
+        listNode1.add(2);
+        listNode1.add(4);
+        listNode1.add(3);
+
+        listNode2.add(5);
+        listNode2.add(6);
+        listNode2.add(4);
+
+//        ListNode listNode1 = new ListNode(234);
+//        ListNode listNode2 = new ListNode(564);
         solution.solution(listNode1, listNode2);
     }
 }
 class Solution{
-    public void solution(ListNode listNode1, ListNode listNode2) {
 
+    public void solution(List<Integer> listNode1, List<Integer> listNode2) {
+        String node1 = "";
+        String node2 = "";
+        int sum = 0;
+
+        for (int i = listNode1.size() - 1; i >= 0; i--) {
+            node1 += String.valueOf(listNode1.get(i));
+        }
+        for (int i = listNode2.size() - 1; i >= 0; i--) {
+            node2 += String.valueOf(listNode2.get(i));
+        }
+
+        sum = convertString(node1, node2);
+    }
+
+    private int convertString(String node1, String node2) {
+        return Integer.valueOf(node1) + Integer.valueOf(node2);
     }
 }
 
