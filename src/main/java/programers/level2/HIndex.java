cbf66402e1a35fe0ee39ea9294647fda637d7538
soop <kimdaesoop@hotmail.com>
2020-03-11 10:56:53 +0900

    programmers h-index about sort

--- src/main/java/programers/level2/HIndex.java
+++ src/main/java/programers/level2/HIndex.java
@@ -1,6 +1,8 @@
 package programers.level2;
 
 
+import leetCode.ReverseInteger;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -8,7 +10,7 @@ import java.util.List;
 public class HIndex {
     public static void main(String[] args) {
         Solution16 solution = new Solution16();
-        int[] num = {0,6,3,1,5};
+        int[] num = {1545, 1126, 323, 1, 2345,12,63};
         System.out.println(solution.solution(num));
     }
 }
@@ -16,6 +18,32 @@ public class HIndex {
 class Solution16{
 
     public int solution(int[] num) {
+        Arrays.sort(num);
+        int answer = 0;
+        int averageH = 0;
+        int averageD = 0;
+
+        for (int i = num.length - 1; i >= 0; i--) {
+            for (int j = 0; j < num.length; j++) {
+                if (num[i] <= num[j]) {
+                    averageD++;
+                }
+                if (num[i] >= num[j]) {
+                    averageH++;
+                }
+            }
+            if (averageD == num[i]) {
+                return averageD;
+            } else if (averageH == num[i]) {
+                return averageH;
+            }
+            averageD = 0;
+            averageH = 0;
+        }
+        return answer;
+    }
+
+    public int solution2(int[] num) {
         Arrays.sort(num);
         int answer = 0;
         for (int i = 0; i < num.length; i++) {
