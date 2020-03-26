64888fefe65d1bd348ac6494c5c9d46f8097b75f
soop <kimdaesoop@hotmail.com>
2020-03-26 21:33:17 +0900

    leetcode Longest Common Prefix

--- src/main/java/leetCode/LongestCommonPrefix.java
+++ src/main/java/leetCode/LongestCommonPrefix.java
@@ -3,23 +3,22 @@ package leetCode;
 public class LongestCommonPrefix {
     public static void main(String[] args) {
         Solution4 solution = new Solution4();
-        String[] strings = {"flower","flow","flight"};
+        String[] strings = {"flower", "flow", "flight"};
         System.out.println(solution.solution(strings));
     }
 }
 
-class Solution4{
+class Solution4 {
     public String solution(String[] strs) {
-        if (strs == null || strs.length == 0) {
-            return "";
-        }
-        for (int i = 0; i < strs[0].length(); i++) {
-            for (int j = 0; j < strs.length; j++) {
-                if (i >= strs[j].length() || strs[0].charAt(i) != strs[j].charAt(i)) {
-                    return strs[0].substring(0, i);
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) {
+                    return "";
                 }
             }
         }
-        return strs[0];
+        return prefix;
     }
 }
