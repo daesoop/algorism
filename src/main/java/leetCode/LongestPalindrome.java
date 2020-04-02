0123a19cee61fc872c76030f281a53764a0961aa
soop <kimdaesoop@hotmail.com>
2020-04-02 23:42:32 +0900

    leetcode Letter Combinations of a Phone Number

--- src/main/java/leetCode/LongestPalindrome.java
+++ src/main/java/leetCode/LongestPalindrome.java
@@ -15,30 +15,15 @@ public class LongestPalindrome {
 
 class Solution14 {
     public String longestPalindrome(String s) {
-
-        if (s == null || "".equals(s)) {
-            return s;
-        }
-
-        int len = s.length();
-
         String answer = "";
         int max = 0;
+        String bs = "";
+        Stack<String> stack = new Stack<>();
+        String[] strings = s.split("");
+        for (int i = 0; i < strings.length - 1; i++) {
+            bs = strings[i];
+            for (int j = i; j < strings.length; j++) {
 
-        boolean[][] dp = new boolean[len][len];
-
-        for (int j = 0; j < len; j++) {
-
-            for (int i = 0; i <= j; i++) {
-
-                boolean judge = s.charAt(i) == s.charAt(j);
-
-                dp[i][j] = j - i > 2 ? dp[i + 1][j - 1] && judge : judge;
-
-                if (dp[i][j] && j - i + 1 > max) {
-                    max = j - i + 1;
-                    answer = s.substring(i, j + 1);
-                }
             }
         }
         return answer;
