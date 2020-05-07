16ede944eaf33ab7da542e3be69687f2a09941d3
soop <kimdaesoop@hotmail.com>
2020-05-07 09:24:49 +0900

    leet code plus one. changed another way of solution. used stack

--- src/main/java/leetCode/PlusOne.java
+++ src/main/java/leetCode/PlusOne.java
@@ -1,48 +1,69 @@
 package leetCode;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 public class PlusOne {
     public static void main(String[] args) {
 
         Solution22 solution = new Solution22();
-        int[] digits = {1,2,9};
+        int[] digits = {2,4,9,3,9};
         solution.plusOne(digits);
     }
 }
 
 class Solution22 {
-    List<Integer> headInteger = new ArrayList<>();
+
+    boolean plus = false;
 
     public int[] plusOne(int[] digits) {
 
+        Stack<Integer> stack = new Stack<>();
         List<Integer> list = new ArrayList<>();
         for (int i = 0; i < digits.length; i++) {
-            if (digits.length - 1 == i) {
-                this.headInteger = parse(digits[i]);
-                for (int j = 0; j < this.headInteger.size(); j++) {
-                    list.add(this.headInteger.get(j));
+            stack.add(digits[i]);
+        }
+
+        if (stack.size() == 1 && stack.peek() == 9) {
+            stack.pop();
+            list.add(0);
+            list.add(1);
+        } else if (stack.peek() == 9 ) {
+            plus = true;
+            list.add(0);
+            stack.pop();
+        } else if (stack.peek() != 9) {
+            list.add(stack.pop() + 1);
+        }
+
+        while (!stack.empty()) {
+            if (plus) {
+                if (stack.peek() == 9) {
+                    list.add(0);
+                    stack.pop();
+                    plus = true;
+                    if (plus && stack.empty()) {
+                        list.add(1);
+                    }
+                } else {
+                    plus = false;
+                    list.add(stack.pop() + 1);
                 }
             } else {
-                list.add(digits[i]);
+                list.add(stack.pop());
             }
         }
 
         int[] answer = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            System.out.println(list.get(i));
-            answer[i] = list.get(i);
+        int num = 0;
+        for (int i = list.size() - 1; i >= 0; i--) {
+            answer[num] = list.get(i);
+            System.out.println(answer[num]);
+            num++;
         }
         return answer;
     }
 
-    public List<Integer> parse(int digit) {
-        List<Integer> list = new ArrayList<>();
-        String s = String.valueOf(digit + 1);
-        for (int i = 0; i < s.length(); i++) {
-            list.add(Integer.parseInt(String.valueOf(s.charAt(i))));
-        }
-        return list;
-    }
 }
